0c5e77ba8e03cf9631598b2f2639813153e7f521
sun04122000 <sunidhi.jindal001@gmail.com>
2022-05-20 15:57:50 +0530

    made the page responsive

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -23,9 +23,9 @@
 
     <a class="navbar-brand c" href="#">C</a>
     <a class="navbar-brand c-pro" href="#">Course Pro</a>
-    <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-    </button> -->
+    </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto">
